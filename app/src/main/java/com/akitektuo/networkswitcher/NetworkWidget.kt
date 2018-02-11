9861baf3cc3893fbc93b054fce6d53c43eb78701
Akitektuo <alexcopindean@yahoo.com>
2018-02-11 14:25:41 +0200

    Added onClick example

--- app/src/main/java/com/akitektuo/networkswitcher/NetworkWidget.kt
+++ app/src/main/java/com/akitektuo/networkswitcher/NetworkWidget.kt
@@ -3,14 +3,20 @@ package com.akitektuo.networkswitcher
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.widget.RemoteViews
+import android.widget.Toast
 
 /**
  * Implementation of App Widget functionality.
  */
 class NetworkWidget : AppWidgetProvider() {
+
+    companion object {
+        private const val CLICK = "click"
+    }
 
     private var is4GActive = true
 
@@ -29,51 +35,48 @@ class NetworkWidget : AppWidgetProvider() {
         // Enter relevant functionality for when the last widget is disabled
     }
 
-    companion object {
 
-        private const val CLICK = "click"
+    private fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
 
-        internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager,
-                                     appWidgetId: Int) {
+        val views = RemoteViews(context.packageName, R.layout.network_widget)
+        views.setInt(R.id.layoutNetwork, "setBackgroundResource", R.drawable.background_green)
+        views.setTextViewText(R.id.textNetwork, "2G")
+        views.setOnClickPendingIntent(R.id.layoutNetwork, getPendingIntent(context, CLICK))
 
-            // Construct the RemoteViews object
-            val views = RemoteViews(context.packageName, R.layout.network_widget)
-//            views.setTextViewText(R.id.appwidget_text, widgetText)
-            views.setInt(R.id.layoutNetwork, "setBackgroundResource", R.drawable.background_green)
-            views.setTextViewText(R.id.textNetwork, "2G")
-            views.setOnClickPendingIntent(R.id.layoutNetwork, getPendingIntent(context, CLICK))
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
 
-            // Instruct the widget manager to update the widget
-            appWidgetManager.updateAppWidget(appWidgetId, views)
-        }
+    private fun changeTo4G(views: RemoteViews) {
+        views.setInt(R.id.layoutNetwork, "setBackgroundResource", R.drawable.background_red)
+        views.setTextViewText(R.id.textNetwork, "4G")
+    }
 
-        private fun changeTo4G(views: RemoteViews) {
-            views.setInt(R.id.layoutNetwork, "setBackgroundResource", R.drawable.background_red)
-            views.setTextViewText(R.id.textNetwork, "4G")
-        }
+    private fun changeTo2G(views: RemoteViews) {
+        views.setInt(R.id.layoutNetwork, "setBackgroundResource", R.drawable.background_green)
+        views.setTextViewText(R.id.textNetwork, "2G")
+    }
 
-        private fun changeTo2G(views: RemoteViews) {
-            views.setInt(R.id.layoutNetwork, "setBackgroundResource", R.drawable.background_green)
-            views.setTextViewText(R.id.textNetwork, "2G")
-        }
-
-        private fun getPendingIntent(context: Context, action: String): PendingIntent {
-            val intent = Intent(context, NetworkWidget::class.java)
-            intent.action = action
-            return PendingIntent.getBroadcast(context, 1, intent, 0)
-        }
+    private fun getPendingIntent(context: Context, action: String): PendingIntent {
+        val intent = Intent(context, javaClass)
+        intent.action = action
+        return PendingIntent.getBroadcast(context, 0, intent, 0)
     }
 
     override fun onReceive(context: Context, intent: Intent) {
         super.onReceive(context, intent)
         if (CLICK == intent.action) {
+            val appWidgetManager = AppWidgetManager.getInstance(context)
             val views = RemoteViews(context.packageName, R.layout.network_widget)
-            if (is4GActive) {
-                changeTo2G(views)
-            } else {
+            val component = ComponentName(context, NetworkWidget::class.java)
+
+//            if (is4GActive) {
+//                changeTo2G(views)
+//            } else {
                 changeTo4G(views)
-            }
-            is4GActive = !is4GActive
+//            }
+//            is4GActive = !is4GActive
+            Toast.makeText(context, "Clicked and 4G is active: $is4GActive", Toast.LENGTH_SHORT).show()
+            appWidgetManager.updateAppWidget(component, views)
         }
     }
 }
